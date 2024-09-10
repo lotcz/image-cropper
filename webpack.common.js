cbad7514f3b853ea1b031d5cf78c3283a114222e
Karel Zavadil <zavadil@incomaker.com>
2024-09-11 01:04:47 +0200

    production build

--- webpack.common.js
+++ webpack.common.js
@@ -2,13 +2,7 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
 	entry: './index.ts',
-	devtool: 'source-map',
-	devServer: {
-		contentBase: './dist',
-		port: 8000
-	},
 	plugins: [
 		new CopyWebpackPlugin({
 			patterns: [
